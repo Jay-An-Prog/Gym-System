102bac9a2bf43b0bcb24913312179a90ae41c924
Jay-An-Prog <mkibutsuji7019@gmail.com>
2025-08-25 01:48:14 +0800

    Add files via upload

---
+++ member-dashboard/main/universal.js
@@ -0,0 +1,14 @@
+//For sidebar toggle
+function toggleSidebar() {
+    document.getElementById("sideBar").classList.toggle("open");
+}
+
+function logout() {
+    // Show confirm dialog
+    let confirmLogout = confirm("Are you sure you want to log out?");
+    
+    if (confirmLogout) {
+        // Redirect to another HTML page (e.g., login.html)
+        window.location.href = "../../login-form/login.html";
+    }
+}
