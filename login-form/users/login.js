f3e68f121bc2a0338a9870b5ba9ef713377346d9
Jay-An-Prog <mkibutsuji7019@gmail.com>
2025-08-28 13:15:25 +0800

    Update login.js

--- login-form/users/login.js
+++ login-form/users/login.js
@@ -3,7 +3,7 @@ document.getElementById("loginForm").addEventListener("submit", function(e) {
 
   const formData = {
     action: "login", // tell Apps Script it's a login request
-    sonic_account: e.target.sonic_account.value,
+    sonic_email: e.target.sonic_account.value,
     user_password: e.target.user_password.value
   };
 
@@ -22,4 +22,5 @@ document.getElementById("loginForm").addEventListener("submit", function(e) {
     }
   })
   .catch(err => console.error("Error:", err));
+
 });
