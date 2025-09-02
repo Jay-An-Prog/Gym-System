da216b25fc894433120a808d6c7dc86ec08aa536
Jay-An-Prog <mkibutsuji7019@gmail.com>
2025-09-03 03:30:48 +0800

    Delete admin-dashboard directory

--- admin-dashboard/main/universal.js
+++
@@ -1,9 +0,0 @@
-function logout() {
-    // Show confirm dialog
-    let confirmLogout = confirm("Are you sure you want to log out?");
-    
-    if (confirmLogout) {
-        // Redirect to another HTML page (e.g., login.html)
-        window.location.href = "../../login-form/login.html";
-    }
-}
