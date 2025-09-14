64301f3b4e2fd25cd40aec797297de92984beb89
Jay-An-Prog <mkibutsuji7019@gmail.com>
2025-09-15 04:22:09 +0800

    Update universal.js

--- assets/js/ui/member-dashboard/universal.js
+++ assets/js/ui/member-dashboard/universal.js
@@ -11,8 +11,12 @@ function logout(bypass = false) {
         ///////////////// Marks the session storage
         sessionStorage.clear();
         
+        
         // Redirect to another HTML page (e.g., login.html)
-        window.location.href = "../../../../pages/portal/login.html";
+        // Get the repo name from the current pathname GitHub
+        const repoName = window.location.pathname.split("/")[1];
+        // Build the correct URL dynamically
+        window.location.href = `/${repoName}/pages/portal/login.html`;
     }
 }
 
@@ -71,4 +75,5 @@ function stopBox() {
 setInterval(() => {
     if (sessionStorage.getItem("loading_box") === "show") startBox(); // Check current state on page load
     if (!sessionStorage.getItem("member_id")) logout(true); // Immediately logout when the member id is undefined or empty
+
 }, 500); // 500 ms = 0.5 seconds
