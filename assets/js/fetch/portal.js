3f79e885c6ef5131da8e817d1caddb31e0bcecb2
Jay-An-Prog <mkibutsuji7019@gmail.com>
2026-01-06 02:56:44 +0800

    Refactor portal.js for user authentication flow

--- assets/js/fetch/portal.js
+++ assets/js/fetch/portal.js
@@ -113,7 +113,7 @@ async function handleUser(user) {
     sessionStorage.setItem("needs_update", "yes"); // Retrieve user info after signed in
     sessionStorage.setItem("portal_loading_box", "hide"); // hide AFTER request finishes
 
-    window.location.href = "./pages/member-dashboard/profile.html";
+    window.location.href = redirectTo("/pages/member-dashboard/profile.html");
 }
 
 // REDIRECT HELPER
@@ -132,3 +132,4 @@ function redirectTo(path) {
     return redirectUrl;
 
 }
+
