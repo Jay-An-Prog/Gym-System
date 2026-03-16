19bf9be0af18e8f9c55bf14533cff6f4c0d60f02
Jay-An-Prog <mkibutsuji7019@gmail.com>
2026-03-16 22:54:25 +0800

    Refactor subscription.js for improved UI handling

--- assets/js/ui/member-dashboard/subscription.js
+++ assets/js/ui/member-dashboard/subscription.js
@@ -123,7 +123,7 @@ function updater() {
             
             const expText = "Expiration Date: " + (function () {
                 let date = new Date(expirationStr.split("T")[0]); 
-                date.setDate(date.getDate() + 1); // add 1 day
+                date.setDate(date.getDate());
                 let mm = String(date.getMonth() + 1).padStart(2, '0');
                 let dd = String(date.getDate()).padStart(2, '0');
                 let yyyy = date.getFullYear();
